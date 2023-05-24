1a5795cf6321896a40dc710331eda249e940b0f4
tutaaa <tinn5247@gmail.com>
2023-05-24 18:02:41 +0700

    newcode

---
+++ fra532_ws/py_pubsub/py_pubsub/publisher_member_function.py
@@ -0,0 +1,68 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+        self.velocity_publisher = self.create_publisher(Twist,'cmd_vel',10)
+     
+
+
+    def timer_callback(self):
+        msg = String()
+
+        vel_msg = Twist()
+        vel_msg.linear.x = 1
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = 0
+
+        msg.data = 'Hello World: %d' % self.i
+        self.publisher_.publish(msg)
+        self.velocity_publisher.publish(vel_msg)
+
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+        
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
